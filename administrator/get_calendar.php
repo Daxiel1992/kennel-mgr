270ca978c809d2a5a9f862393d872d39eeff06c5
Daxiel1992 <dbdanbench@gmail.com>
2021-12-10 12:33:44 -0700

    Oops forgot to add comments so I don't get confused cause me big dumb dumb :)

--- administrator/get_calendar.php
+++ administrator/get_calendar.php
@@ -16,16 +16,20 @@
 	// Print the date and display the reservations with all relevant information
 	echo $calDate . "<br>";
 
+	// Create our appointments array
 	$appointments = array();
 
+	// Go through each reservation found for the selected day
 	while($reservation = $get_reservations->fetch_assoc()) {
+		// Grab our relationships between the reservations from our reservations_pets table
 		$get_relations = $db->query("SELECT * FROM `reservations_pets` WHERE `reservation_id` = '{$reservation['id']}'");
 		
 		while($relation = $get_relations->fetch_assoc()) {
 			$get_pets = $db->query("SELECT `name`, `breed` FROM `pets` WHERE `id` = '{$relation['pet_id']}'");
 			$get_kennels = $db->query("SELECT `name` FROM `kennels` WHERE `id` = '{$relation['kennel_id']}'");
 			$get_clients = $db->query("SELECT `first_name`, `last_name` FROM `clients` WHERE `id` = '{$reservation['client_id']}'");	
-		
+
+			// Set all of our necessary variables from our queries
 			while($kennel = $get_kennels->fetch_assoc()) {
 				$kennel_name = $kennel['name'];
 			}
@@ -35,7 +39,8 @@
 			while($client = $get_clients->fetch_assoc()) {
 				$client_name = $client['first_name'] . " " . $client['last_name'];
 			}
-
+			
+			// Loop through our $appointments array and if there are dogs staying in the same kennel together, don't create another entry, but just combine the two.
 			if(count($appointments) > 0) {
 				foreach ($appointments as &$a) {
 					if($a['kennel_name'] == $kennel_name AND $a['reservation_id'] == $reservation['id'] AND $a['pet'] != $pet) {
@@ -51,15 +56,16 @@
 				}
 
 			} else {
+				// If there are no entries, just add one.
 				$appointment = array("reservation_id"=>$reservation['id'], "kennel_name"=>$kennel_name, "client_name"=>$client_name, "pet"=>$pet, "start_date"=>$reservation['start_date'], "end_date"=>$reservation['end_date']);
 				$appointments[] = $appointment;
 			}
+			// Unset our variable that tells us if we already made an entry
 			unset($added);
 		}
-
-		
 	}
 
+	// Loop through our appointments and echo out our info
 	foreach($appointments as $appointment) {
 		echo "{$appointment['reservation_id']} | {$appointment['kennel_name']} | {$appointment['client_name']} | {$appointment['pet']} | Starting: {$appointment['start_date']} - {$appointment['end_date']}";
 		echo "<br>";
