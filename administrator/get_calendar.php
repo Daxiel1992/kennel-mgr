7c21f8ff56543948444c7b41501c33f01541e388
Daxiel1992 <dbdanbench@gmail.com>
2021-12-09 21:47:00 -0700

    Added the ability to select pets and multiple kennels for the same reservations

--- administrator/get_calendar.php
+++ administrator/get_calendar.php
@@ -11,24 +11,36 @@
 	}
 	
 	// Grab all reservations that are current for that day and order them by start date
-	$reservations = $db->query("SELECT * FROM `reservations` WHERE `start_date` <= '$calDate' AND `end_date` >= '$calDate' ORDER BY `start_date` ASC");
+	$get_reservations = $db->query("SELECT * FROM `reservations` WHERE `start_date` <= '$calDate' AND `end_date` >= '$calDate' ORDER BY `start_date` ASC");
 
 	// Print the date and display the reservations with all relevant information
 	echo $calDate . "<br>";
 
-	while($reservation = $reservations->fetch_assoc()) {
-		$kennels = $db->query("SELECT `id`, `name` FROM `kennels` WHERE `id` = '{$reservation['kennel_id']}'");
-		$clients = $db->query("SELECT `id`, `first_name`, `last_name` FROM `clients` WHERE `id` = '{$reservation['client_id']}'");
+	while($reservation = $get_reservations->fetch_assoc()) {
+		$relations = $db->query("SELECT * FROM `reservations_pets` WHERE `reservation_id` = '{$reservation['id']}'");
+			while($relation = $relations->fetch_assoc()) {
+				$get_pets = $db->query("SELECT `name`, `breed` FROM `pets` WHERE `id` = '{$relation['pet_id']}'");
+				$get_kennels = $db->query("SELECT `name` FROM `kennels` WHERE `id` = '{$relation['kennel_id']}'");
+				$get_clients = $db->query("SELECT `first_name`, `last_name` FROM `clients` WHERE `id` = '{$reservation['client_id']}'");
 
-		while($kennel = $kennels->fetch_assoc()) {
-			$kennel_name = $kennel['name'];
-		}
+				while($pets_array = $get_pets->fetch_assoc()) {
+					$pets[] = $pets_array;
+				}
+
+				while($kennel = $get_kennels->fetch_assoc()) {
+					$kennel_name = $kennel['name'];
+				}
 	
-		while($client = $clients->fetch_assoc()) {
-			$client_name = $client['first_name'] . " " . $client['last_name'];
-		}	
+				while($client = $get_clients->fetch_assoc()) {
+					$client_name = $client['first_name'] . " " . $client['last_name'];
+				}	
 		
-		echo $reservation['id'] . " | " . $kennel_name . " | " . $client_name . " | Starting: " . $reservation['start_date'] . " Ending: " . $reservation['end_date'] . "<br>";
+				echo $reservation['id'] . " | " . $kennel_name . " | " . $client_name . " | "; foreach($pets as $pet){echo $pet['name'] . " - " . $pet['breed'] . " | ";} echo "Starting: " . $reservation['start_date'] . " Ending: " . $reservation['end_date'] . "<br>";
+
+				unset($pets);
+
+			}	
+
 	}
 ?>
 
